f5ae6984d76c0ca6555c3439ab22e0fd2d070c83
Theyvison <theyvison.jf@hotmail.com>
2025-08-30 16:30:35 -0300

    Service layer, component registration

--- src/main/java/com/theyvison/course/resources/UserResource.java
+++ src/main/java/com/theyvison/course/resources/UserResource.java
@@ -1,17 +1,31 @@
 package com.theyvison.course.resources;
 
 import com.theyvison.course.entities.User;
+import com.theyvison.course.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController // indica que essa classe e um controlador, um controlador e responsável por receber as requisições
 @RequestMapping(value = "/users") // caminho do recurso
 public class UserResource {
+    @Autowired
+    private UserService userService;
+
     @GetMapping // indica que esse metodo responde a requisições do tipo GET
-    public ResponseEntity<User> findAll() {
-        User u = new User(1L, "Maria", "maria@gmail.com", "(99)99999-9999", "123456");
-        return ResponseEntity.ok().body(u);
+    public ResponseEntity<List<User>> findAll() {
+        List<User> list = userService.findAll();
+        return ResponseEntity.ok().body(list);
+    }
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<User> findById(@PathVariable Long id) {
+        User obj = userService.findById(id);
+        return ResponseEntity.ok().body(obj);
     }
 }
---
+++ src/main/java/com/theyvison/course/services/UserService.java
@@ -0,0 +1,24 @@
+package com.theyvison.course.services;
+
+import com.theyvison.course.entities.User;
+import com.theyvison.course.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service // essa classe e um servico, classe de servico -> responsável por implementar as regras de negocio
+public class UserService {
+    @Autowired
+    private UserRepository userRepository;
+
+    public List<User> findAll() {
+        return userRepository.findAll();
+    }
+
+    public User findById(Long id) {
+        Optional<User> obj = userRepository.findById(id);
+        return obj.get();
+    }
+}

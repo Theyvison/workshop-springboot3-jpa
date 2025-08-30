ad414a67776ab70dbc2bd51f97b5d51491c30732
Theyvison <theyvison.jf@hotmail.com>
2025-08-30 17:14:22 -0300

    Order, Instant, ISO 8601

---
+++ src/main/java/com/theyvison/course/repositories/OrderRepository.java
@@ -0,0 +1,7 @@
+package com.theyvison.course.repositories;
+
+import com.theyvison.course.entities.Order;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface OrderRepository extends JpaRepository<Order, Long> {
+}

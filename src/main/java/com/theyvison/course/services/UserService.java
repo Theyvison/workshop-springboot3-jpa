12fb24a6c2f65792257b6b9a3cdeef519356207d
Theyvison <theyvison.jf@hotmail.com>
2025-08-31 12:08:37 -0300

    Exception handling - findById

--- src/main/java/com/theyvison/course/services/UserService.java
+++ src/main/java/com/theyvison/course/services/UserService.java
@@ -2,6 +2,7 @@ package com.theyvison.course.services;
 
 import com.theyvison.course.entities.User;
 import com.theyvison.course.repositories.UserRepository;
+import com.theyvison.course.services.exceptions.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -19,7 +20,7 @@ public class UserService {
 
     public User findById(Long id) {
         Optional<User> obj = userRepository.findById(id);
-        return obj.get();
+        return obj.orElseThrow(() -> new ResourceNotFoundException(id));
     }
 
     public User insert(User obj) {
